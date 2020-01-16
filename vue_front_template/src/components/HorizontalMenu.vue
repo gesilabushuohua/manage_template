f4a89098255a9ab2f47d16bd0dbea3dfad7063dc
gesilabushuohua <370225203@qq.com>
2020-01-16 22:45:29 +0800

    水平菜单，js控制，改为css控制，添加根据路由变化改变菜单

--- vue_front_template/src/components/HorizontalMenu.vue
+++ vue_front_template/src/components/HorizontalMenu.vue
@@ -1,27 +1,43 @@
 /* 
-  水平菜单导航栏
+  @function 二级水平菜单导航栏
   @prop menus 菜单列表
   menus:[
     {
       cName:'导航名', 必填
       path:'路径',
-      children:[]
+      children:[
+         {
+              cName: "子菜单一",
+              path: "/subpath"
+          }
+      ]
     }
   ]
   
  */
 <template>
-  <ul class="menu">
+  <ul class="menus" @click.capture="handleMenuClick">
     <li
       v-for="(menu,index) in menus"
       :id="menu.path"
       :key="index"
-      @mouseenter="handelMenuEnter"
-      @mouseleave="handleMenuLeave"
-      @click="handleMenuClick"
+      class="menu"
+      :data-path="menu.path"
+      :data-is-leaf="!hasMenuChild(menu)"
     >
-      <span class="text">{{ menu.cName }}</span>
-      <span class="icon el-icon-arrow-down"></span>
+      <span class="text" :data-path="menu.path" :data-is-leaf="!hasMenuChild(menu)">{{ menu.cName }}</span>
+      <span v-show="hasMenuChild(menu)" class="icon el-icon-arrow-down"></span>
+      <ul v-if="hasMenuChild(menu)" class="subMenu">
+        <li
+          v-for="(subMenu,subi) in menu.children"
+          :id="subMenu.path"
+          :key="index+subi"
+          class="text"
+          data-is-leaf="true"
+          data-has-parent="true"
+          :data-path="subMenu.path"
+        >{{ subMenu.cName }}</li>
+      </ul>
     </li>
   </ul>
 </template>
@@ -43,75 +59,111 @@ export default {
   computed: {},
   watch: {},
   created() {},
-  mounted() {},
+  mounted() {
+    this.initMenuActive();
+  },
   methods: {
-    initMenuActive() {},
-
     /* 
-    @function 鼠标移入，箭头图标翻转
+    @function 判断 munu 是否有子菜单
     */
-    handelMenuEnter(event) {
-      let [, icon] = event.target.children;
-      if (!icon) {
+    hasMenuChild(menu) {
+      return menu.children && menu.children.length > 0;
+    },
+    initMenuActive() {
+      const { path } = this.$route;
+      const menu = document.getElementById(path);
+      this.handleActivedMenu(menu);
+    },
+    /* 
+    @function 为菜单添加样式，移除样式
+    @param 添加样式 dom 节点
+    */
+    handleActivedMenu(menu) {
+      if (!menu) {
         return;
       }
-      if (icon.classList.contains(rotateClassName)) {
-        return;
+      const { hasParent } = menu.dataset;
+      if (hasParent) {
+        const { parentElement } = menu.parentElement;
+        parentElement.classList.toggle(menuActivedName);
       }
-      icon.classList.add(rotateClassName);
+      menu.classList.toggle(menuActivedName);
     },
 
-    /* 
-    @function 鼠标移出，箭头图标恢复原样
-    */
-    handleMenuLeave(event) {
-      let [, icon] = event.target.children;
-      if (!icon) {
-        return;
-      }
-      if (icon.classList.contains(rotateClassName)) {
-        icon.classList.remove(rotateClassName);
-      }
-    },
     handleMenuClick(event) {
       let { target } = event;
-    },
-    removeLastActived() {
-      let [last] = document.getElementsByName(menuActivedName);
-      if (!last) {
+      const { path: lastPath } = this.$route;
+      const { path, isLeaf } = target.dataset;
+      if (!isLeaf || !path || path === lastPath) {
         return;
       }
+      const menu = document.getElementById(path);
+      const lastMenu = document.getElementById(lastPath);
+      this.$router.push(path);
+      //  移除之前菜单激活样式  
+      this.handleActivedMenu(lastMenu);
+      //  添加当前菜单激活样式
+      this.handleActivedMenu(menu);
     }
   }
 };
 </script>
 <style lang="scss" scoped>
 $actived-color: #3c80ff;
+$font-color: #fff;
 
-.menu {
+.menus {
   position: relative;
   display: flex;
   margin: 0;
   list-style: none;
-  color: #fff;
+  font-size: 14px;
+  color: $font-color;
 
-  > li {
+  .menu {
     display: block;
     width: 100px;
     text-align: center;
     line-height: 50px;
     border-bottom: 4px solid transparent;
 
-    .text {
+    > .text {
       margin-right: 10px;
     }
 
-    .icon {
-      transition: all 0.2s;
+    > .icon {
+      transition: all 0.4s;
     }
 
-    .icon-rotate {
-      transform: rotate(180deg);
+    .subMenu {
+      display: none;
+      margin-top: 5px;
+      padding: 0;
+      list-style: none;
+      color: #000;
+      background-color: #fff;
+      border: 1px solid #ebeef5;
+      box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
+      transition: all 0.4s;
+
+      .actived {
+        color: $actived-color;
+      }
+    }
+  }
+
+  .menu:hover {
+    > .icon {
+      transform: rotate(-180deg);
+    }
+
+    .subMenu {
+      display: block;
+
+      > li:hover {
+        cursor: pointer;
+        background-color: regba($actived-color, 0.2);
+      }
     }
   }
 
