dc1d2b8d1041b2d7feb179af5588e413471fa183
gesila <370225203@qq.com>
2020-02-29 17:09:41 +0800

    add form template

--- vue_front_template/src/router/index.js
+++ vue_front_template/src/router/index.js
@@ -1,11 +1,17 @@
+/*
+ * @Description: des
+ * @Date: 2020-01-03 17:45:59
+ * @Author: LB
+ */
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 
-const Login = () => import('@/views/Login');
-const Layout = () => import('@/views/Layout');
-const About = () => import('@/views/About');
-const Home = () => import('@/views/Home');
-const TableBuilderEle = () => import('@/views/builder/TableBuilderEle');
+const Login = () => import('@/views/Login.vue');
+const Layout = () => import('@/views/Layout.vue');
+const About = () => import('@/views/About.vue');
+const Home = () => import('@/views/Home.vue');
+const TableBuilderEle = () => import('@/views/builder/TableBuilderEle.vue');
+const FormTemplateEle = () => import('@/views/template/FormTemplateEle.vue');
 
 Vue.use(VueRouter);
 
@@ -36,6 +42,11 @@ const routes = [
     path: '/tableBuilderEle',
     name: 'tableBuilderEle',
     component: TableBuilderEle
+  },
+  {
+    path: '/formTemEle',
+    name: 'FormTemplateEle',
+    component: FormTemplateEle
   }
 ];
 
