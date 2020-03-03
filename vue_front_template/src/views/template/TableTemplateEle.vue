a318eaa39abd87ce89d99bb1b89dc8b937480045
gesila <370225203@qq.com>
2020-03-03 17:33:51 +0800

    update table template

--- vue_front_template/src/views/template/TableTemplateEle.vue
+++ vue_front_template/src/views/template/TableTemplateEle.vue
@@ -5,14 +5,54 @@
  -->
 <template>
   <div class="wrapper">
-    <el-table :data="tableData" style="width: 100%">
-      <column-plus v-for="(col,index) in columns" :key="index" :col-config="col" />
-    </el-table>
+    <div class="main">
+      <el-table :data="tableData">
+        <column-plus
+          v-for="(col,index) in columns"
+          :key="index"
+          :col-config="col"
+          :edit-fn="handleEditTableData"
+          :del-fn="handleDelTableData"
+        />
+      </el-table>
+    </div>
+    <div class="footer">
+      <el-pagination
+        :current-page="currentPage"
+        :page-sizes="[20, 40, 60]"
+        :page-size="pageSize"
+        :total="total"
+        layout="total, prev, pager, next, jumper, sizes"
+        @size-change="changePageSize"
+        @current-change="changeCurrentPage"
+      />
+    </div>
   </div>
 </template>
 
 <script>
 import { ColumnPlus } from './tableColumnPlus.js';
+import { tableMixin } from '@/mixis/tableMixin.js';
+
+// 表格增删查改函数
+const getTableDataFn = () =>
+  new Promise(resolve => {
+    resolve();
+  });
+const addTableDataFn = () =>
+  new Promise(resolve => {
+    resolve();
+  });
+const editTableDataFn = () =>
+  new Promise(resolve => {
+    resolve();
+  });
+const delTableDataFn = () =>
+  new Promise(resolve => {
+    resolve();
+  });
+
+//  表格行列设置
 const columns = [
   {
     label: '时间',
@@ -27,7 +67,7 @@ const columns = [
     renderCells: [
       {
         ele: 'img',
-        src: 'aaaaa'
+        prop: 'src'
       }
     ]
   },
@@ -41,56 +81,138 @@ const columns = [
       {
         ele: 'btn',
         type: 'text',
-        disabled: true,
+        opt: 'edit',
+        addClass: 'add',
+        disabled: false,
         text: '编辑'
       },
       {
         ele: 'btn',
         icon: 'el-icon-edit',
-        text: '删除'
+        text: '删除',
+        opt: 'del'
       }
     ]
   }
 ];
+
 export default {
   name: 'TableTemPlateEle',
   components: { ColumnPlus },
   props: {},
+  mixins: [
+    //  封装表格常用函数
+    tableMixin
+  ],
   data() {
     return {
       columns,
-      tableData: [
-        {
-          date: '2016-05-02',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1518 弄'
-        },
-        {
-          date: '2016-05-04',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1517 弄'
-        },
-        {
-          date: '2016-05-01',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1519 弄'
-        },
-        {
-          date: '2016-05-03',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1516 弄'
-        }
-      ]
+      tableData: [],
+      currentPage: 1,
+      pageSize: 20,
+      total: 100
     };
   },
   computed: {},
   watch: {},
-  created() {},
+  created() {
+    this.tableData = [
+      {
+        date: '2016-05-02',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄',
+        src: 'aaa'
+      },
+      {
+        date: '2016-05-04',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1517 弄',
+        src:
+          'http://img4.imgtn.bdimg.com/it/u=1223780803,2412931209&fm=26&gp=0.jpg'
+      },
+      {
+        date: '2016-05-01',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1519 弄',
+        src: 'aaa'
+      },
+      {
+        date: '2016-05-03',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1516 弄',
+        src: 'aaa'
+      }
+    ];
+  },
   mounted() {},
-  methods: {}
+  methods: {
+    changePageSize(size) {
+      console.log('changePageSize');
+      this.pageSize = size;
+      this.requsetGetTableData();
+    },
+    changeCurrentPage(cur) {
+      console.log('changeCurrentPage');
+      this.currentPage = cur;
+      this.requsetGetTableData();
+    },
+    requsetGetTableData() {
+      if (!getTableDataFn) {
+        return;
+      }
+      const { currentPage, pageSize } = this;
+      const param = {
+        currentPage,
+        pageSize
+      };
+      console.log('getTableDataFn');
+      getTableDataFn(param)
+        .then(res => {})
+        .catch(err => {});
+    },
+    requsetAddTableData() {
+      if (!addTableDataFn) {
+        return;
+      }
+      const param = {};
+      addTableDataFn(param)
+        .then(res => {})
+        .catch(err => {});
+    },
+    requsetEditTableData() {
+      if (!editTableDataFn) {
+        return;
+      }
+      const param = {};
+      editTableDataFn(param)
+        .then(res => {})
+        .catch(err => {});
+    },
+    requsetDelTableData() {
+      if (!delTableDataFn) {
+        return;
+      }
+      const param = {};
+      delTableDataFn(param)
+        .then(res => {})
+        .catch(err => {});
+    },
+
+    handleEditTableData(row) {
+      console.log('handleEditTableData', row);
+    },
+    handleDelTableData(row) {
+      console.log('handleDelTableData', row);
+    }
+  }
 };
 </script>
 <style lang="scss" scoped>
-.wrapper {
+.main {
+  height: 80vh;
+}
+
+.footer {
+  text-align: center;
 }
 </style>
--- vue_front_template/src/views/template/tableColumnPlus.js
+++ vue_front_template/src/views/template/tableColumnPlus.js
@@ -3,17 +3,32 @@
  * @Date: 2020-03-02 11:48:19
  * @Author: LB
  */
+
 import Vue from 'vue';
 
-//  暂时只支持 按钮、图片类型
 
+
+
+//  作废，UI 组件 class 易作废
 const btn = {
   functional: true,
   name: 'btn',
   render(h, context) {
     const cell = context.props.data;
-    return <el-button {...{ attrs: cell }}>{cell.text}</el-button>;
+    const { addClass, row, optFn } = cell;
+    // 避免组件样式被声明样式覆盖，重新定义赋值
+    const reClass = addClass ? addClass : '';
+    delete cell.addClass;
+
+    if (cell.optFn) {
+      delete cell.optFn;
+      delete cell.row;
+
+      return <el-button {...{ attrs: cell }} class={reClass} onClick={() => optFn(row)} >{cell.text}</el-button>;
+    }
+    return <el-button {...{ attrs: cell }} class={reClass} >{cell.text}</el-button>;
   }
+
 };
 
 const img = {
@@ -21,6 +36,7 @@ const img = {
   name: 'btn',
   render(h, context) {
     const cell = context.props.data;
+    console.log(context);
     return <img {...{ attrs: cell }} />;
   }
 };
@@ -30,34 +46,89 @@ const cellType = {
   img
 };
 
+
+
 export const ColumnPlus = Vue.component('ColumnPlus', {
   name: 'ColumnPlus',
+  /* 
+    目前，只支持 编辑，删除功能
+  */
   props: {
     colConfig: {
       type: Object,
       required: true
+    },
+    editFn: {
+      type: Function,
+      default: null
+    },
+    delFn: {
+      type: Function,
+      default: null
     }
   },
   render(h) {
     const { colConfig } = this;
     const { renderCells } = colConfig;
+    const { render } = colConfig;
 
+    //  作废
+    // 渲染 renderCells 元素 
     if (renderCells && renderCells.length > 0) {
       delete colConfig.renderCells;
+      const that = this;
       return (
         <el-table-column
           {...{ attrs: colConfig }}
           {...{
             scopedSlots: {
               default: scope => {
-                console.log(scope);
                 return renderCells.map(cell => {
                   const item = cellType[cell.ele];
-                  if (cell.ele === 'img' && cell.prop) {
-                    item.src = scope.row[cell.prop];
+                  const copy = { ...cell };
+                  if (copy.ele === 'img' && copy.prop) {
+                    copy.src = scope.row[copy.prop];
                   }
-                  return <item data={cell} />;
+
+                  if (copy.ele === 'btn' && cell.opt) {
+                    if (cell.opt === 'edit') {
+                      copy.optFn = this.editFn;
+                    }
+
+                    if (cell.opt === 'del') {
+                      copy.optFn = this.delFn;
+                    }
+                  }
+                  copy.row = scope.row;
+                  return <item data={copy} />;
                 });
+              }
+            }
+          }}
+        ></el-table-column>
+      );
+    }
+
+    //  渲染 render 函数,暂时无法解决 多按钮绑定事件
+    if (render) {
+      delete colConfig.renderCells;
+      const item = {
+        functional: true,
+        name: 'item',
+        render: (h, context) => {
+          const { row, $index: index } = context.props.data;
+          return render(h, { row, index });
+        }
+      };
+
+
+      return (
+        <el-table-column
+          {...{ attrs: colConfig }}
+          {...{
+            scopedSlots: {
+              default: scope => {
+                return <item data={scope} />;
               }
             }
           }}
@@ -68,3 +139,47 @@ export const ColumnPlus = Vue.component('ColumnPlus', {
     return <el-table-column {...{ attrs: colConfig }} />;
   }
 });
+
+
+/*
+Dome 事例
+render，renderCells 只支持其中一种
+
+ {
+    label: '头像',
+    render:(h,{row,index})=>{
+      return (<img src={row.src}/>)
+    }
+  }
+
+  //  作废
+  {
+    label: '抓怕',
+    renderCells:[
+      {
+        ele: 'img',
+        prop:'src'
+      }
+    ]
+  }
+
+  //  作废
+  {
+    label: '操作',
+    renderCells: [
+      {
+        ele: 'btn',
+        type: 'text',
+        disabled: true,
+        text: '编辑'
+      },
+      {
+        ele: 'btn',
+        icon: 'el-icon-edit',
+        text: '删除'
+      }
+    ]
+  }
+
+
+*/
